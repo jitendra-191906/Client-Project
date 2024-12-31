484c16e8dfe89266eed25d40da06aa90b691a519
Jitendra kumar <jitendrakumar8080397415@gmail.com>
2024-12-31 22:08:03 +0530

    Component and style sheet added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React from 'react';
+import Header from './Component/Header';
+import About from './Component/About';
+import Products from './Component/Products';
+import Contact from './Component/Contact';
+import Footer from './Component/Footer';
+import Feature from './Component/Feature';
+import Testimonials from './Component/Testimonials';
+
 
 function App() {
   return (
     <>
-      
+      <Header />
+      <About />
+      <Products />
+      <Feature/>
+      <Testimonials/>
+      <Contact />
+      <Footer />
     </>
   );
 }
+
 
 export default App;
---
+++ src/Component/Feature.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Feature() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Feature
