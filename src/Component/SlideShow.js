67cde7b5e955a29b979a85edd131e52222b6a8a6
Jitendra kumar <jitendrakumar8080397415@gmail.com>
2025-01-04 21:22:38 +0530

    slide show added

---
+++ src/Component/SlideShow.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import '../Style/slideshow.css';
+import img1 from '../Images/GraniteFlooring.jpeg';
+import img2 from '../Images/TileFlooring.jpg';
+import img3 from '../Images/image3.jpg';
+
+const SlideShow = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+
+  return (
+    <div className="slideshow-container">
+      <Slider {...settings}>
+        <div>
+          <img src={img1} alt="Slide 1" />
+        </div>
+        <div>
+          <img src={img2} alt="Slide 2" />
+        </div>
+        <div>
+          <img src={img3} alt="Slide 3" />
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default SlideShow;
