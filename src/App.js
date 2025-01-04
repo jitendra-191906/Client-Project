c26e20f59f80718c5c36d41e68c06dbce8389b18
Jitendra kumar <jitendrakumar8080397415@gmail.com>
2025-01-04 21:26:23 +0530

    new library 'react-slick' added and use for slide show

--- src/App.js
+++ src/App.js
@@ -7,14 +7,16 @@ import Footer from './Component/Footer';
 import Feature from './Component/Feature';
 import Testimonials from './Component/Testimonials';
 import Categories from './Component/Categories';
+import SlideShow from './Component/SlideShow';
 
 function App() {
   return (
     <>
       <Header />
+      <SlideShow/>
+      <Categories/>
       <About />
       <Product />
-      <Categories/>
       <Feature/>
       <Testimonials/>
       <Contact />
--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../Style/header.css';
-import img1 from '../Images/Logo.png'
+import img1 from '../Images/Logo.png';
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
       <nav className="navbar" id="header">
         <div className="navbar-logo">
           <img src={img1} alt="Company Logo" />
         </div>
-        <ul className="navbar-links">
+
+        {/* Hamburger Icon */}
+        <div className="hamburger" onClick={toggleMenu}>
+          <div className={`line ${isMenuOpen ? 'open' : ''}`}></div>
+          <div className={`line ${isMenuOpen ? 'open' : ''}`}></div>
+          <div className={`line ${isMenuOpen ? 'open' : ''}`}></div>
+        </div>
+
+        {/* Navbar Links */}
+        <ul className={`navbar-links ${isMenuOpen ? 'active' : ''}`}>
           <li><a href="#header">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#products">Products</a></li>
@@ -18,8 +33,11 @@ function Header() {
         </ul>
         
       </nav>
+      <div className="hero">
+        <h1>Welcome to Jaipur Tiles & Granite to Beautify Your Space</h1>
+      </div>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
