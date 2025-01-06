6b7f57e3ae168d12fb0b413df77e9a301980a0f7
Deepak Garg <garg882001@gmail.com>
2025-01-06 12:45:40 +0530

    Refactoring Footer

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -13,6 +13,7 @@ import Linkedin from '../Images/linkedin.png';
 const Footer = () => {
   return (
     <footer className="footer">
+
       <div className="footer-top-sec ">
         <div className="footer-logo-section">
           <div className="footer_logo_img">
@@ -22,25 +23,47 @@ const Footer = () => {
               className="footer-logo-img"
             />
           </div>
+
           <div className="footer-company-text">
-            Jaipur Tiles and granites to provide elegant look to your space
-          </div>
-          <div className="social_media">
-            <div className="icon">
-              <img src={Instagram} alt="Instagram" />
-            </div>
-            <div className="icon">
-              <img src={Youtube} alt="Youtube" />
-            </div>
-            <div className="icon">
-              <img src={Whatsapp} alt="Whatsapp" />
-            </div>
-            <div className="icon">
-              <img src={Linkedin} alt="Linkedin" />
-            </div>
+            <p>
+              Jaipur Tiles and granites to provide elegant look to your space
+            </p>
           </div>
 
+          <div className="social_media">
+            <div className="social-media-inner">
+              <div className="social-media-icon-sec">
+                <img
+                  src={Instagram}
+                  alt="Instagram"
+                  className="social-media-icon"
+                />
+              </div>
+              <div className="social-media-icon-sec">
+                <img
+                  src={Youtube}
+                  alt="Youtube"
+                  className="social-media-icon"
+                />
+              </div>
+              <div className="social-media-icon-sec">
+                <img
+                  src={Whatsapp}
+                  alt="Whatsapp"
+                  className="social-media-icon"
+                />
+              </div>
+              <div className="social-media-icon-sec">
+                <img
+                  src={Linkedin}
+                  alt="Linkedin"
+                  className="social-media-icon"
+                />
+              </div>
+            </div>
+          </div>
         </div>
+
         <div className="footer_about_section">
           <div className="about_item"> Home</div>
           <div className="about_item"> Products</div>
@@ -48,47 +71,56 @@ const Footer = () => {
           <div className="about_item"> Contact Us</div>
           <div className="about_item"> Sitemap</div>
         </div>
+
         <div className="footer_contact_section">
+
           <div className="foocon">
             <div className="foocon_img">
-              <img src={ContactImg} alt="contact logo" />
-
+              <img
+                src={ContactImg}
+                alt="contact logo"
+              />
             </div>
-            <div className="foocon_text">
-
-              +91 9694317171 / 9694177771
-            </div>
-
+            <p className="foocon_text">
+              +91 9694317171 | +91 9694177771
+            </p>
           </div>
-          <div className="foocon">
-            <div className="foocon_img">
-              <img src={LocationImg} alt="location logo" />
-            </div>
-            <div className="foocon_text">
-              <p> Near HP Petrol Pump,<br />
-                Near Bharat Petrol Pump,<br />
-                Kalwad Road, Hathoj, Jaipur</p>
 
-            </div>
-          </div>
           <div className="foocon">
             <div className="foocon_img">
-              <img src={EmailImg} alt="email logo" />
+              <img
+                src={LocationImg}
+                alt="location logo"
+              />
             </div>
-            <div className="foocon_text">
+            <p className="foocon_text">
+              Near HP Petrol Pump,
+              Near Bharat Petrol Pump,
+              Kalwad Road, Hathoj, Jaipur
+            </p>
+          </div>
+
+          <div className="foocon">
+            <div className="foocon_img">
+              <img
+                src={EmailImg}
+                alt="email logo"
+              />
+            </div>
+            <p className="foocon_text">
               jaipurtilesandgranite@gmail.com
-            </div>
+            </p>
           </div>
-          
-          
+
         </div>
       </div>
-      <hr className="hrtag" />
+
       <div className="footer-bottom-sec">
         <p>
           &copy; 2025 All rights reserved.
         </p>
       </div>
+
     </footer>
   );
 };
