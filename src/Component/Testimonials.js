ca4fab009319fba476b151408b4607bb206ce550
Jitendra kumar <jitendrakumar8080397415@gmail.com>
2025-01-12 11:27:01 +0530

    tesimonial slider added

--- src/Component/Testimonials.js
+++ src/Component/Testimonials.js
@@ -6,96 +6,89 @@ import customer3 from '../Images/cust3.jpg';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import '../Style/slideshow.css';
 
 const customerReviewsList = [
   {
     name: "Bessie Cooper",
     profileImg: customer1,
-    reviewText: "best experience and best quality of tiles. I have brought tiles and marble for my complete home.",
+    reviewText: "Best experience and best quality of tiles. I have brought tiles and marble for my complete home.",
   },
   {
-    name: "Bessie Cooper",
-    profileImg: customer1,
-    reviewText: "best experience and best quality of tiles. I have brought tiles and marble for my complete home.",
+    name: "Ralph Edwards",
+    profileImg: customer2,
+    reviewText: "Great customer service and excellent marble designs. Truly satisfied with the purchase.",
   },
   {
-    name: "Bessie Cooper",
-    profileImg: customer1,
-    reviewText: "best experience and best quality of tiles. I have brought tiles and marble for my complete home.",
+    name: "Annette Black",
+    profileImg: customer3,
+    reviewText: "The quality of tiles is unmatched. The team was very professional throughout.",
   },
   {
-    name: "Bessie Cooper",
+    name: "Cameron Williamson",
     profileImg: customer1,
-    reviewText: "best experience and best quality of tiles. I have brought tiles and marble for my complete home.",
+    reviewText: "Beautiful designs and excellent quality! Highly recommend this store.",
   },
-
-]
+  {
+    name: "Eleanor Pena",
+    profileImg: customer2,
+    reviewText: "Affordable pricing and great quality tiles. Perfect for any home renovation!",
+  },
+];
 
 function Testimonials() {
-
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024, // Tablets
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768, // Mobile
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
-
   return (
-    <>
-      <div className="testimonials_main">
-        <div className="testimonial_inner">
-          <div className="testimonials_text">
-            <h2 className="website_heading_text">
-              “Our clients often tell us.....
-            </h2>
-          </div>
-          <div className="testimonial_bottom">
-            {/* <div className="slider-container">
-
-
-            <Slider {...settings}> */}
-            {
-              customerReviewsList.map((customer, index) => {
-
-                return (
-                  <React.Fragment key={index + 1}>
-
-                    <div className="customer_review">
-                      <div className="customer_review_inner">
-
-                        <div className="customer_details">
-                          <img
-                            src={customer1}
-                            alt=""
-                            className="img_customer"
-                          />
-                          <span className="cutomer_name" >Bessie Cooper</span>
-                        </div>
-
-                        <hr className="hr_heading" />
-
-                        <div className="customer_review_text">
-                          <p>best experience and best quality of tiles. I have brought tiles and marble for my complete home. </p>
-                        </div>
-                      </div>
-                    </div>
-                  </React.Fragment>
-                )
-              })
-            }
-
-            {/* </Slider>
-
-          </div> */}
-          </div>
-        </div>
+    <div className="testimonials_main">
+      <div className="testimonial_inner">
+        <h2 className="website_heading_text">“Our clients often tell us.....”</h2>
+        <Slider {...settings} className="testimonial_slider">
+          {customerReviewsList.map((customer, index) => (
+            <div key={index} className="customer_review">
+              <div className="customer_review_inner">
+                <div className="customer_details">
+                  <img
+                    src={customer.profileImg}
+                    alt={`${customer.name}'s profile`}
+                    className="img_customer"
+                  />
+                  <span className="customer_name">{customer.name}</span>
+                </div>
+                <hr className="hr_heading" />
+                <div className="customer_review_text">
+                  <p>{customer.reviewText}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </Slider>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default Testimonials
-
+export default Testimonials;
