a3f2a3031103de833231a7c7e8e2efdeed2b4e70
Jitendra kumar <jitendrakumar8080397415@gmail.com>
2025-01-20 16:38:20 +0530

    marble product section slider and images added

--- src/Component/Products/MarbleProducts.js
+++ src/Component/Products/MarbleProducts.js
@@ -1,7 +1,22 @@
 import React from 'react'
 import "../../Style/products.css"
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import img1 from "../../Images/white_marble.jpeg";
+import img2 from "../../Images/travertine_marble.jpeg";
+import img3 from "../../Images/onyx_marble.jpeg";
+import img4 from "../../Images/statuario_marble.jpeg";
+import img5 from "../../Images/Italian_marble.jpeg";
+import img6 from "../../Images/black_marquina_marble.jpeg";
+import img7 from "../../Images/Grey_William_Italian_Marble.jpeg";
+import img8 from "../../Images/Emperador_Marble.jpeg";
 
-import img1 from "../../Images/black_marble.png";
+
+import image1 from "../../Images/marble_flooring_1.jpg";
+import image2 from "../../Images/marble_flooring_2.jpeg";
+import image3 from "../../Images/marble_flooring_3.jpeg";
+
 
 const productsList = [
 
@@ -10,30 +25,88 @@ const productsList = [
         name: "White Marble"
     },
     {
-        image: img1,
-        name: "Black Marble"
+        image: img2,
+        name: "Travertine Marble"
+    },
+   
+    {
+        image: img3,
+        name: "Onyx Marble"
     },
     {
-        image: img1,
-        name: "Red Marble"
+        image: img4,
+        name: "Statuario Marble"
     },
     {
-        image: img1,
-        name: "Green Marble"
+        image: img5,
+        name: "Italian Marble"
     },
     {
-        image: img1,
-        name: "Blue Marble"
+        image: img6,
+        name: "Black Marquina Marble"
+    },
+    {
+        image: img7,
+        name: " Grey William Italian Marble"
+    },
+    {
+        image: img8,
+        name: "Emperador Marble"
     },
 ]
 
+const slides = [
+    {
+      image: image1,
+      title: "Elegant Granite Flooring",
+    },
+    {
+      image:image3,
+      title: "Modern Tile Designs",
+    },
+    {
+      image:image2,
+      title: "Custom Flooring Solutions",
+    },
+  ];
 const MarbleProducts = () => {
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: false, // No navigation arrows for simplicity
+      };
+    
     return (
         <>
+ <div className="testimonials_slideshow">
+      <Slider {...settings}>
+        {slides.map((slide, index) => (
+          <div key={index} className="testimonial_slide">
+            <div
+              className="testimonial_slide_bg"
+              style={{
+                backgroundImage: `url(${slide.image})`,
+              }}
+            >
+              <div className="testimonial_text_overlay">
+                {/* <h2 className="testimonial_slide_title">{slide.title}</h2> */}
+                {/* <p className="testimonial_slide_description">{slide.description}</p> */}
+              </div>
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+        
 
             <div className='products-page'>
                 <div className='products-page-inner'>
-                    <div> slide show</div>
                     <div>
                         <h2 className='website_heading_text'>
                             Explore Marbel Products
