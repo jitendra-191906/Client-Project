970dad13d36e583e5158367ecce775c0ca390ba7
Jitendra kumar <jitendrakumar8080397415@gmail.com>
2025-01-22 01:12:20 +0530

    show more button refactor

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -14,16 +14,16 @@ import Granite from './Granite'
 const Home = () => {
     return (
         <>
-            <SlideShow />
-            <Categories />
+            {/* <SlideShow /> */}
+            {/* <Categories /> */}
             {/* <Marble/> */}
             {/* <Granite/> */}
             {/* <Tiles/> */}
             <Product />
             {/* <About /> */}
-            <Feature />
-            <Letconnect />
-            <Testimonials />
+            {/* <Feature /> */}
+            {/* <Letconnect /> */}
+            {/* <Testimonials /> */}
         </>
     )
 }
