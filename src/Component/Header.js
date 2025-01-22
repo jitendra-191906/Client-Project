83492e9ecd578558e828c7bbba22b391bdbe97db
Deepak Garg <garg882001@gmail.com>
2025-01-22 13:49:31 +0530

    Updated: Header UI

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,22 +1,31 @@
 import React, { useState } from 'react';
 import '../Style/header.css';
 import img1 from '../Images/Logo_Image/Logo.png';
+import { CiMenuKebab } from "react-icons/ci";
+import { MdClose } from "react-icons/md";
 
-import { ABOUT_US, TESTIMONIALS, LETS_CONNECT, HEADER, MARBELS, TILES, GRANITES, PRODUCTS } from '../constants';
+import { ABOUT_US, TESTIMONIALS, LETS_CONNECT, HEADER, MARBELS, TILES, GRANITES, PRODUCTS, CATEGORIES } from '../constants';
+
 import { useScrollToComponent } from './helper';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function Header() {
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  
-    const scrollToComponent = useScrollToComponent();
+  const navigate = useNavigate();
+
+  const location = useLocation();
+
+  const { pathname } = location;
+
+  const scrollToComponent = useScrollToComponent();
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
- 
+
   const closeAllMenus = () => {
     setIsMenuOpen(false);
   };
@@ -27,73 +36,130 @@ function Header() {
         id={HEADER}
         className="navbar"
       >
-        <div className="navbar_logo">
-          <img
-            src={img1}
-            alt="Company Logo"
-            className="logo"
-          />
-        </div>
+        <div className='navbar_inner'>
 
-        <div 
-        className="menu_icon" 
-        onClick={toggleMenu}
-        >
-          {
-          isMenuOpen 
-          ? (
-            <span className="cross">&times;</span>
-          ) 
-          : (
-            <>
-              <span className="dot"></span>
-              <span className="dot"></span>
-              <span className="dot"></span>
-            </>
-          )}
-        </div>
-
-        <div className={`navbar_links ${isMenuOpen ? 'open' : ''}`}>
-          <div
-            onClick={() => {
-              scrollToComponent(ABOUT_US);
-              closeAllMenus();
-            }}
-            className="navbar_items"
-          >
-            About
+          <div className="navbar_logo">
+            <img
+              src={img1}
+              alt="Company Logo"
+              className="logo"
+            />
           </div>
 
-          <div 
-          className="navbar_items" 
-          onClick={() => {
-            // toggleProductsDropdown();
-            scrollToComponent(PRODUCTS);
-            closeAllMenus();
+          <div className='header_inner_right'>
+            <div
+              className="header_icon_menu"
+            >
+              {
+                isMenuOpen
+                  ? (
+                    <div className='icon_menu_dropdown'>
 
-          }
-          }>
-            Products
+                      <div className='icon_menu_dropdown_close'>
+                        <div onClick={() => toggleMenu()}>
+                          <MdClose
+                            fill='black'
+                            size={26}
+                          />
+                        </div>
+                      </div>
+
+                      {
+                        pathname !== "/" &&
+                        (
+                          <div
+                            className="navbar_items"
+                            onClick={() => {
+                              navigate("/");
+                              closeAllMenus();
+                            }}
+                          >
+                            Home
+                          </div>
+                        )
+                      }
+
+                      <div
+                        className="navbar_items"
+                        onClick={() => {
+                          scrollToComponent(CATEGORIES);
+                          closeAllMenus();
+                        }}
+                      >
+                        Products
+                      </div>
+
+                      <div
+                        onClick={() => {
+                          scrollToComponent(TESTIMONIALS);
+                          closeAllMenus();
+                        }}
+                        className="navbar_items"
+                      >
+                        Testimonials
+                      </div>
+
+                      <div
+                        onClick={() => {
+                          scrollToComponent(LETS_CONNECT);
+                          closeAllMenus();
+                        }}
+                        className="navbar_items"
+                      >
+                        Contact
+                      </div>
+                    </div>
+                  )
+                  : (
+                    <div onClick={() => toggleMenu()}>
+                      <CiMenuKebab
+                        fill='black'
+                        size={26}
+                      />
+                    </div>
+                  )
+              }
+
+            </div>
+
+            <div className="open_links_section">
+
+              {
+                pathname !== "/" &&
+                (
+                  <div
+                    className="navbar_items"
+                    onClick={() => navigate("/")}
+                  >
+                    Home
+                  </div>
+                )
+              }
+
+              <div
+                className="navbar_items"
+                onClick={() => scrollToComponent(CATEGORIES)}
+              >
+                Products
+              </div>
+
+              <div
+                onClick={() => scrollToComponent(TESTIMONIALS)}
+                className="navbar_items"
+              >
+                Testimonials
+              </div>
+
+              <div
+                onClick={() => scrollToComponent(LETS_CONNECT)}
+                className="navbar_items"
+              >
+                Contact
+              </div>
+
+            </div>
           </div>
 
-          <div
-            onClick={() => {
-              scrollToComponent(TESTIMONIALS);
-              closeAllMenus();
-            }}
-            className="navbar_items"
-          >
-            Testimonials
-          </div>
-          <div
-            onClick={() => {
-              scrollToComponent(LETS_CONNECT);
-              closeAllMenus();
-            }}
-            className="navbar_items"
-          >
-            Contact
-          </div>
         </div>
       </nav>
     </>
